27d43348bf7bfd1423f73f11012118f6e342a75d
T <gauquelintimothe@gmail.com>
2020-08-04 13:48:22 +0200

    Add Log In & Log Out Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { authConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sso-app';
+
+   constructor(private oauthService: OAuthService) {
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login(){
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout(){
+    this.oauthService.logOut();
+  }
 }
